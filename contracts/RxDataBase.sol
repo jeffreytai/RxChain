26edd5cb31b834f8243e82e7853a56590b949628
Jeffrey Tai <jeffreytai@ucla.edu>
2018-02-18 00:14:41 -0800

    refactored code for base entities

--- contracts/RxDataBase.sol
+++ contracts/RxDataBase.sol
@@ -9,24 +9,34 @@ contract RxDataBase is Ownable, AccessControl {
     uint256 totalMedication;
 
     // Write data
-    function registerManufacturer(address _manufacturerAddr, string _name) onlyAuthorized public;
+    function registerManufacturer(address _manufacturerAddr, bytes16 _name) onlyAuthorized public;
     function removeManufacturer(address _manufacturerAddr) onlyAuthorized public;
-    function registerWholesaler(address _wholesalerAddr, string _name) onlyAuthorized public;
+    function registerWholesaler(address _wholesalerAddr, bytes16 _name) onlyAuthorized public;
     function removeWholesaler(address _wholesalerAddr) onlyAuthorized public;
-    function registerPharmacy(address _pharmacyAddr, string _name) onlyAuthorized public;
+    function registerPharmacy(address _pharmacyAddr, bytes16 _name) onlyAuthorized public;
     function removePharmacy(address _pharmacyAddr) onlyAuthorized public;
     function registerPatient(address _patientAddr) public;
     function removePatient(address _patientAddr) public;
     function registerMedication(
         address manufacturerAddr,
-        address wholesalerAddr,
-        uint64 expirationDate,
-        uint64 manufactureCreationDate,
-        uint64 wholesaleReceiptDate,
         bytes32 serialNumber,
         uint8 wholesalePrice,
         uint8 pharmacyPrice,
         uint8 patientPrice) onlyManufacturer public;
+    function removeMedication(uint256 medicationId) onlyManufacturer public;
+    function createPrescription(
+        uint256 _medicationId,
+        address _wholesalerAddr,
+        address _pharmacyAddr,
+        address _patientAddr,
+        uint64 _expirationDate,
+        uint64 _manufactureCreationDate,
+        uint64 _wholesaleReceiptDate,
+        uint64 _pharmacyReceiptDate,
+        uint64 _patientReceiptDate) onlyPharmacy public;
+    function removePrescription(uint256 _prescriptionId) public onlyPharmacyOrPatient
+
 
     // Read data
+
 }
